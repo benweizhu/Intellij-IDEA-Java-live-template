566c7de4b99d2265454a724fc2b21a7eff6a8f4e
Benwei Zhu <benweizhu@gmail.com>
2019-01-31 15:05:57 +0800

    init spring java project

---
+++ src/main/java/me/zeph/intellij/live/livetemplatetesting/LiveTemplateTestingApplication.java
@@ -0,0 +1,14 @@
+package me.zeph.intellij.live.livetemplatetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTemplateTestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiveTemplateTestingApplication.class, args);
+  }
+
+}
+
